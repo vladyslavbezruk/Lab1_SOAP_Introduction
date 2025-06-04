62e613722536e79a172ba48d3984d70f2273d384
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2025-06-04 23:38:35 +0300

    Done.

--- src/test/java/com/lab1/lab1/Lab1SoapIntroductionApplicationTests.java
+++ src/test/java/com/lab1/lab1/Lab1SoapIntroductionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Lab1SoapIntroductionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
